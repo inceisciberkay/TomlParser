70021b31ded6d060317a7836a92765e5f0e088e2
berkay inceisci <inceisciberkay@gmail.com>
2022-07-26 18:01:56 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/berkay/projects/esen-project
+# Build directory: /home/berkay/projects/esen-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
