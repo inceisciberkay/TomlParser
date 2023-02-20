ee8962a087e50a62fd9597fc8751f28ddc8d159a
inceisciberkay <110044051+inceisciberkay@users.noreply.github.com>
2023-02-20 23:43:42 +0300

    delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/berkay/projects/esen-project
-# Build directory: /home/berkay/projects/esen-project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
